9e7eb218f6fe857413af45c8fd79588c94dae80b
algowhiz <147070075+algowhiz@users.noreply.github.com>
2024-02-08 22:12:36 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,10 +6,10 @@ import { IoCall } from "react-icons/io5";
 const Contact = () => {
     return (
         <>
-            <div class="container mx-auto py-10" name="Contact">
-                <h2 class="text-3xl font-bold mb-6 text-center">Get in Touch</h2>
+            <div className="container mx-auto py-10" name="Contact">
+                <h2 className="text-3xl font-bold mb-6 text-center">Get in Touch</h2>
 
-                <div class="flex flex-nowrap justify-center space-x-2 md:space-x-5 ">
+                <div className="flex flex-nowrap justify-center space-x-2 md:space-x-5 ">
                     <a href="https://www.linkedin.com/in/manishkadam1/" target="_blank" >
                         <FaLinkedin size={80} className='border-2 p-2 rounded cursor-pointer hover:bg-[#243070]' />
                     </a>
@@ -25,27 +25,41 @@ const Contact = () => {
                 </div>
 
 
-                <div name="contact" className='w-auto h-[600px] flex justify-center items-center p-1 contact'>
-                    <form action="https://getform.io/f/8a6ce622-d082-4360-9eb2-746d74f99c27" method='POST' className='flex flex-col max-w-[400px] w-full'>
-                        <input type="text" placeholder='Name' name='name' className='rounded my-4 p-2 bg-[#ccd6f6]' />
-                        <input type="email" placeholder='email' name='email' className='rounded my-4 p-2  bg-[#ccd6f6]' />
-                        <textarea name="message" cols="30" rows="10" className='rounded my-4 p-4  bg-[#ccd6f6]' placeholder='message'></textarea>
-                        <button className='text-white border-2 rounded hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center '>Let's Collaborate</button>
+
+                <div className="min-h-screen bg-[rgb(1, 14, 53)] flex justify-center items-center ">
+                    <form action="https://getform.io/f/8a6ce622-d082-4360-9eb2-746d74f99c27" method='POST' className="bg-[rgb(1, 14, 53)]  shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4">
+                        <div className="mb-4">
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                                id="name" name='name' type="text" placeholder="Your Name" />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                id="email" name='email' type="email" placeholder="Email" />
+                        </div>
+                        <div className="mb-6">
+                            <textarea
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                id="message" name='message' placeholder="Message" rows="5"></textarea>
+                        </div>
+                        <div className="flex items-center justify-center ">
+                            <button
+                                className= "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit">
+                                Let's Collaborate
+                            </button>
+                        </div>
                     </form>
                 </div>
 
 
-                <div class="flex flex-col md:flex-row justify-between ">
 
-                    <div class="flex flex-col mb-4 md:mb-0 ml-4">
-                        <span class="text-lg font-semibold text-[#e9dbdb]">Email:</span>
-                        <p class="text-[#e9dbdb] ">manishkadam219@gmail.com</p>
-                    </div>
+                <div className="flex flex-col md:flex-row justify-between ">
 
-
-                    <div class="flex flex-col ml-4">
-                        <span class="text-lg font-semibold text-[#e9dbdb]">Phone:</span>
-                        <p class="text-[#e9dbdb]">+91 9137884101</p>
+                    <div className="flex flex-col mb-4 md:mb-0 ml-4">
+                        <span className="text-lg font-semibold text-[#e9dbdb]">Email:</span>
+                        <p className="text-[#e9dbdb] ">manishkadam219@gmail.com</p>
                     </div>
                 </div>
 
